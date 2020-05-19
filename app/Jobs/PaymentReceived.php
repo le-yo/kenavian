ff2a94c34634f8d21448885f006d04541b59d2b8
Le-Yo <lenykoskey@gmail.com>
2020-05-19 20:30:21 +0300

    Reintroducing saf and air

--- app/Jobs/PaymentReceived.php
+++ app/Jobs/PaymentReceived.php
@@ -135,19 +135,19 @@ class PaymentReceived extends Job implements ShouldQueue
 
         $telco = ValidatePhoneController::channel($payment_data['account_no']);
 
-        if($telco == 'AIRTEL'){
+        if($telco == 'AIRTEL' || strtolower(substr($payment_data['account_no'],0,3))=='air'){
             $commission_kenavian = 0.06*$payment_data['amount'];
             $commission_yangu = 0.02*$payment_data['amount'];
             $serviceId = $services['102']['serviceID'];
             $serviceCode = $services['102']['serviceCode'];
             $phone = "254".substr(trim($payment_data['account_no']),-9);
-        }elseif($telco == 'TELKOM'){
+        }elseif($telco == 'TELKOM' || strtolower(substr($payment_data['account_no'],0,3))=='tel'){
             $commission_kenavian = 0.05*$payment_data['amount'];
             $commission_yangu = 0.02*$payment_data['amount'];
             $serviceId = $services['103']['serviceID'];
             $serviceCode = $services['103']['serviceCode'];
             $phone = "254".substr(trim($payment_data['account_no']),-9);
-        }elseif(strtolower(substr($payment_data['account_no'],0,5))=='TOKEN'){
+        }elseif(strtolower(substr($payment_data['account_no'],0,5))=='TOKEN' || strtolower(substr($payment_data['account_no'],0,3))=='tok'){
             $commission_kenavian = 0.014*$payment_data['amount'];
             $commission_yangu = 0.005*$payment_data['amount'];
             $serviceId = $services['105']['serviceID'];
@@ -169,7 +169,7 @@ class PaymentReceived extends Job implements ShouldQueue
 
 
         $data['Request'] = [
-            "transactionRef"=>"kenavian_".$payment_data['transaction_id'],
+            "transactionRef"=>"kenavian_".$payment_data['transaction_id']."_".rand(1,200),
             "serviceID"=>$serviceId,
             "serviceCode"=>$serviceCode,
             "msisdn"=>$phone,
